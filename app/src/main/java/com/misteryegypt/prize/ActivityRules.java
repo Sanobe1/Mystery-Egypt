9d67e6f6583d882f6faca3b3ac02d2ffd8890cfe
Sanobe1 <valentenkos@gmail.com>
2021-07-28 12:59:24 +0300

    dlkm

--- app/src/main/java/com/misteryegypt/prize/ActivityRules.java
+++ app/src/main/java/com/misteryegypt/prize/ActivityRules.java
@@ -1,5 +1,9 @@
 package com.misteryegypt.prize;
 
+import android.content.Intent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +12,21 @@ public class ActivityRules extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_rules);
     }
+
+    public void onClick(View v) {
+        Button button = findViewById(v.getId());
+
+        switch (button.getTag().toString()) {
+
+            case "back": {
+                startActivity(new Intent(this, StartActivity.class));
+                finish();
+                break;
+            }
+        }
+    }
 }
+
